485382940223f58dd69eff01818f2042d2082f38
Michael Livshits <livsmichael@gmail.com>
2026-03-25 00:29:35 +0200

    add SDK exports: Mill class + all converters importable as library

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { Mill } from "./mill.js";
+export type { Converter, ConversionResult, StreamInfo } from "./types.js";
+export { PdfConverter } from "./converters/pdf.js";
+export { DocxConverter } from "./converters/docx.js";
+export { HtmlConverter } from "./converters/html.js";
+export { CsvConverter } from "./converters/csv.js";
+export { JsonConverter } from "./converters/json.js";
+export { XlsxConverter } from "./converters/xlsx.js";
+export { PlainTextConverter } from "./converters/plain-text.js";
